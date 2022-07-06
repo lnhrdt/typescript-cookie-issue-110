5139576433986cf098ff1caba245f62077f4a8cb
Leonhardt Koepsell <hello@lnhrdt.com>
2022-07-05 22:59:43 -0500

    Demonstrate issue with typescript-cookie

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {getCookie} from 'typescript-cookie';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>{getCookie('my-cookie') || 'cookie not set'}</p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
